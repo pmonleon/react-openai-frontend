ccd42fc9a975f84ed9518249a544c86d2fe28786
Francisco Monleon <paco.monleon@octoenergy.com>
2025-03-12 16:04:44 +0100

    fin seccion 10

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -2,4 +2,5 @@ export * from './orthography.use-case';
 export * from './pros-cons-stream-generator.use-case';
 export * from './pros-cons-stream.use-case';
 export * from './pros-cons.use-case';
-export * from './translate-text.use-case'
+export * from './translate-text.use-case';
+export * from './text-to-audio.use-case';
--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -2,6 +2,7 @@
 export * from './chat-bubbles/GptMessage';
 export * from './chat-bubbles/MyMessage';
 export * from './chat-bubbles/GptOrthographyMessage';
+export * from './chat-bubbles/GptMessageAudio';
 
 export * from './chat-input-boxes/TextMessageBox';
 export * from './chat-input-boxes/TextMessageBoxFile';
